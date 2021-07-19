e87416151add3f395315a3c719e16ef9ead2fc45
Ndegwa <dennisndegwa001@gmail.com>
2021-07-20 00:53:38 +0300

    Edit comment to read charts

--- backend/routes/charts.js
+++ backend/routes/charts.js
@@ -1,15 +1,12 @@
 var express = require('express');
-// const chartController = require('../controllers/chartController');
-// var data = require('../mockdata/mock.json')
 const data = require('../charts/index');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET charts listing. */
 router.get('/', function(req, res, next) {
   console.log('\nobj:', JSON.stringify(data, null, 2));
   res.send(data);
 });
 
-// router.get('/', chartController.getAll);
 
 module.exports = router;
