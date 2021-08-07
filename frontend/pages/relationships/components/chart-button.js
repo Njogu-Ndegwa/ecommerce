fcf7a370a09c02b9c0d16d0e7871005f5c00fcc6
Dohd <alloicebrian@gmail.com>
2021-08-07 23:44:06 +0300

    Adjust indentation

--- frontend/pages/relationships/components/chart-button.js
+++ frontend/pages/relationships/components/chart-button.js
@@ -2,21 +2,15 @@ import {useState} from 'react';
 import {Card, Select, Button, Row, Col, Modal } from 'antd';
 import { PlayCircleOutlined, LineChartOutlined} from '@ant-design/icons';
 const { Meta } = Card;
-import {
-  ComposedChart,
-  Line,
-  Label,
-  LabelList,
-  Bar,
-  XAxis,
-  YAxis,
-  Legend,
+import { 
+  ComposedChart, Line, Label, LabelList, Bar,
+  XAxis, YAxis, Legend,
 } from "recharts";
 import Item from 'antd/lib/list/Item';
 const {Option} = Select
 
 export default function PlotChart({data, columns}) {
-  console.log(data) 
+  console.log('plot_data: ', {data, columns}) 
   const [isModalVisible, setisModalVisible] = useState(false)
   const [graphType, setGraphType] = useState('line_graph')
   const [yColumn, setyColumn] = useState(columns[1].dataIndex)
@@ -34,7 +28,6 @@ export default function PlotChart({data, columns}) {
 
   const [ Ylabel, setYlabel ] = useState('Y-axis')
   const handleYColumnChange = (value) => {
-    console.log('y_col: ', value)
     columns.forEach(v => {
       if (v.dataIndex === value) setYlabel(v.title)
     })
@@ -44,82 +37,81 @@ export default function PlotChart({data, columns}) {
     setGraphType(value)
   }
 
-  const handleY2Change = () => {
+  const handleY2Change = () => {}
 
-  }
-console.log((graphType === 'line_graph' || graphType === 'composite_graph') && 'amen')
+    console.log((graphType === 'line_graph' || graphType === 'composite_graph') && 'amen')
+
     return (
-        <Card style={{ width: 200, marginBottom: 16 }}>
-                <Row>
-      <Col span={6}>
-      <Button type="primary" icon={<PlayCircleOutlined />}/>
-      </Col>
-      <Col span={6} offset={12}>
-      <Button type="primary" onClick={handleShowChart} icon={<LineChartOutlined />}/>
-      </Col>
-    </Row>
-      <Modal
-      title="Basic Modal" 
-      visible={isModalVisible}
-      centered
-      width={1000}
-      onOk={handleOk} 
-      onCancel={handleCancel}
-      >
-                <Card title='New Plot' style={{ width: 900, margin: "20px" }}>
-            <div style={{display: "flex", justifyContent:"space-between" }} >
-                <div>
-                <span>Y1 Column</span>
-                </div>
-                {graphType === 'composite_graph' && <div style={{marginRight:"75px"}} >
-                <span>Y2 Column</span>
-                </div>}
-            </div>
-            <Row>
-                <Col span={12}>
-            <Select defaultValue="lucy" style={{width: 120}} onChange={handleYColumnChange}>
-              {columns.map((item) => (
-                <Option value={item.dataIndex}>{item.title}</Option>
-              ))}
-            </Select>
-                </Col>
-                <Col span={4} offset={8}>
-            <Select defaultValue="lucy" style={{width: 120}} onChange={handleY2Change} >
-            {columns.map((item) => (
-              item != 
-                <Option value={item.dataIndex}>{item.title}</Option>
-              ))}
-            </Select>
-                </Col>
-            </Row>
-            <div style={{padding: "20px"}}>
-<ComposedChart width={800} height={320} data={data}>
-    <XAxis dataKey = {columns[0].dataIndex} >
-    <Label value="Occurence" offset={0} position="insideBottom"/>
-    </XAxis>
-    <YAxis dataKey={yColumn} domain = {[0, 'dataMax']} >
-    <Label value="Conversion  rate to next completed Order (%)" angle={-90} position="insideBottomLeft" />
-    </YAxis>
-    {graphType === 'composite_graph' && <YAxis yAxisId="right" dataKey='tco' orientation='right' tickLine={false} axisLine={false} domain={[0, 100]} >
-    <Label value="Total Completed  Orders" angle={-90} position="insideTopRight" />
-    </YAxis>}
-    {((data && graphType === 'bar_graph') ||( data && graphType === 'composite_graph')) && <Bar dataKey={yColumn} barSize={60} fill="#8884d8" >
-    <LabelList dataKey="cto" position="insideTop"/>
-    </Bar>}
-    {/* <Bar dataKey={yColumn} barSize={60} fill="#8884d8" ></Bar> */}
-    {(graphType === 'line_graph' || graphType === 'composite_graph') && <Line dataKey={yColumn} stroke="#000" fill="#000"  strokeDasharray="3 3"/> }
-    {/* <Line dataKey={yColumn} stroke="#000" fill="#000"  strokeDasharray="3 3"/> */}
-    <Legend verticalAlign="top" height={36}/>
-  </ComposedChart>
-  </div>
+      <Card style={{ width: 200, marginBottom: 16 }}>
+        <Row>
+          <Col span={6}>
+            <Button type="primary" icon={<PlayCircleOutlined />}/>
+          </Col>
+          <Col span={6} offset={12}>
+            <Button type="primary" onClick={handleShowChart} icon={<LineChartOutlined />}/>
+          </Col>
+        </Row>
+        <Modal
+          title="New Plot" 
+          visible={isModalVisible}
+          centered
+          width={1000}
+          onOk={handleOk} 
+          onCancel={handleCancel}
+        >
+          <Card style={{ width: 900, margin: "20px" }}>
+          <div style={{display: "flex", justifyContent:"space-between" }} >
+              <div>
+              <span>YColumns</span>
+              </div>
+              {graphType === 'composite_graph' && <div style={{marginRight:"75px"}} >
+              <span>Y2 Column</span>
+              </div>}
+          </div>
+          <Row>
+              <Col span={12}>
+          <Select defaultValue="--select column--" onChange={handleYColumnChange} style={{width: 120}} >
+            {columns.map(item => (
+              <Option value={item.dataIndex} key={item.key}>
+                {item.title}
+              </Option>
+            ))}
+          </Select>
+              </Col>
+              <Col span={4} offset={8}>
+          <Select defaultValue="Refresh plot" style={{width: 120}} onChange={handleY2Change} >
+            {/* Refresh plot Button */}
+          </Select>
+              </Col>
+          </Row>
+          <div style={{padding: "20px"}}>
+          <ComposedChart width={800} height={320} data={data}>
+          <XAxis dataKey = {columns[0].dataIndex} >
+          <Label value="Occurence" offset={0} position="insideBottom"/>
+          </XAxis>
+          <YAxis dataKey={yColumn} domain = {[0, 'dataMax']} >
+          <Label  value={Ylabel} angle={-90} position="insideBottomLeft" />
+          </YAxis>
+          {graphType === 'composite_graph' && <YAxis yAxisId="right" dataKey='tco' orientation='right' tickLine={false} axisLine={false} domain={[0, 100]} >
+          <Label value="Total Completed  Orders" angle={-90} position="insideTopRight" />
+          </YAxis>}
+          {((data && graphType === 'bar_graph') ||( data && graphType === 'composite_graph')) && <Bar dataKey={yColumn} barSize={60} fill="#8884d8" >
+          <LabelList dataKey="cto" position="insideTop"/>
+          </Bar>}
+          {/* <Bar dataKey={yColumn} barSize={60} fill="#8884d8" ></Bar> */}
+          {(graphType === 'line_graph' || graphType === 'composite_graph') && <Line dataKey={yColumn} stroke="#000" fill="#000"  strokeDasharray="3 3"/> }
+          {/* <Line dataKey={yColumn} stroke="#000" fill="#000"  strokeDasharray="3 3"/> */}
+          <Legend verticalAlign="top" height={36}/>
+          </ComposedChart>
+          </div>
 
-    <Select defaultValue="lucy" style={{width: 120}} onChange={ handleGraphTypeChange}>
-                <Option value="composite_graph">Composite Graph</Option>
-                <Option value="bar_graph">Bar Graph</Option>
-                <Option value="line_graph">Line Graph</Option>
-    </Select>
-  </Card>
-      </Modal>
+          <Select defaultValue="lucy" style={{width: 120}} onChange={ handleGraphTypeChange}>
+            <Option value="composite_graph">Composite Graph</Option>
+            <Option value="bar_graph">Bar Graph</Option>
+            <Option value="line_graph">Line Graph</Option>
+          </Select>
+          </Card>
+        </Modal>
       </Card>
     )
 }
