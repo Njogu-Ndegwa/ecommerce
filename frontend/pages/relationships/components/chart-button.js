7fa676fc12418fa934fd2355d18d4819024eccbe
Dohd <alloicebrian@gmail.com>
2021-08-10 09:45:27 +0300

    Update chart logic in chart-button

--- frontend/pages/relationships/components/chart-button.js
+++ frontend/pages/relationships/components/chart-button.js
@@ -9,24 +9,13 @@ import {
 const {Option} = Select
 
 export default function PlotChart({data, columns, xAxis}) {
-const [isModalVisible, setisModalVisible] = useState(false)
-const [graphType, setGraphType] = useState('line_graph')
+  const [isModalVisible, setisModalVisible] = useState(false);
+  const [graphType, setGraphType] = useState('line_graph');
 
-  const handleShowChart = () => {
-    setisModalVisible(true)
-  }
-
-  const handleOk = () => {
-    setisModalVisible(false)
-  }
-
-  const handleCancel = () => {
-    setisModalVisible(false)
-  }
-
-  const handleGraphTypeChange = (value) => {
-    setGraphType(value)
-  }
+  const [ Ylabel, setYlabel ] = useState('Total Revenue Impact');
+  const [ Yvalue, setYvalue] = useState('total_revenue_impact');
+  const [ Y2label, setY2label ] = useState('Total Revenue Impact');
+  const [ Y2value, setY2value] = useState('total_revenue_impact');
 
   // columns store
   const [store, setStore] = useState({
@@ -35,38 +24,53 @@ const [graphType, setGraphType] = useState('line_graph')
   });
 
   useEffect(() => {
-    // filter 'customer' in yColumns
-    const filteredCols = columns.filter(obj => obj.dataIndex !== 'customer');
-    setStore({yAxis: filteredCols, y2Axis: columns});
+    const list = [
+      'total_revenue_impact',
+      'total_primary',
+      'total_secondary',
+      'coversion_rate',
+      'avarage_days'
+    ];
+    const cols = columns.filter(obj => list.includes(obj.dataIndex));
+    // console.log("cols", cols);
+    setStore({yAxis: cols, y2Axis: cols});
   }, [columns]);
-
-  const [ Ylabel, setYlabel ] = useState('Total Revenue Impact');
-  const [ Yvalue, setYvalue] = useState('total_revenue_impact');
-  const [ Y2value, setY2value] = useState('customer');
 
   const onYchange = index => {
     const cols = columns.filter(obj => obj.dataIndex !== index);
     store.yAxis.forEach(obj => {
-      if (obj.dataIndex === index && obj.dataIndex !== xAxis.dataIndex) {
+      if (obj.dataIndex === index) {
         setYlabel(obj.title);
         setYvalue(obj.dataIndex);
       }
     });
-    setStore(prev => ({...prev, xAxis: cols}));
+    setStore(prev => ({...prev, y2Axis: cols}));
   };
 
   const onY2change = index => {
-    const cols = columns.filter(obj => (
-      obj.dataIndex !== index && 
-      obj.dataIndex !== xAxis.dataIndex
-    ));
+    const cols = columns.filter(obj => obj.dataIndex !== index);
     store.y2Axis.forEach(obj => {
-      if (obj.dataIndex === index && obj.dataIndex !== xAxis.dataIndex) {
+      if (obj.dataIndex === index) {
+        setY2label(obj.title);
         setY2value(obj.dataIndex);
       }
     });
     setStore(prev => ({...prev, yAxis: cols}));
   };
+
+  // modal logic
+  const handleShowChart = () => {
+    setisModalVisible(true)
+  }
+  const handleOk = () => {
+    setisModalVisible(false)
+  }
+  const handleCancel = () => {
+    setisModalVisible(false)
+  }
+  const handleGraphTypeChange = (value) => {
+    setGraphType(value)
+  }
 
 // const ticks = [5000, 10000, 15000, 20000, 25000, 60000, 70000, 80000, 90000, 100000, 110000, 120000]
   return (
@@ -98,7 +102,7 @@ const [graphType, setGraphType] = useState('line_graph')
         </div>
         <Row>
           <Col span={12}>
-            <Select defaultValue={columns[1]?.title} onChange={onYchange} style={{width: '250px'}} >
+            <Select defaultValue={store.yAxis[0]?.title} onChange={onYchange} style={{width: '250px'}} >
               {store.yAxis.map(item => (
                 <Option value={item.dataIndex} key={item.key}>
                   {item.title}
@@ -107,7 +111,7 @@ const [graphType, setGraphType] = useState('line_graph')
             </Select>
           </Col>
           <Col span={4} offset={4}>
-            <Select defaultValue={columns[0]?.title} onChange={onY2change} style={{width: '250px'}} >
+            <Select defaultValue={store.y2Axis[0]?.title} onChange={onY2change} style={{width: '250px'}} >
               {store.y2Axis.map(item => (
                 <Option value={item.dataIndex} key={item.key}>
                   {item.title}
@@ -117,20 +121,23 @@ const [graphType, setGraphType] = useState('line_graph')
           </Col>
         </Row>
         <div style={{padding: "20px"}}>
+          <h2 style={{fontWeight: 'bold'}}>{`${Ylabel} by ${xAxis.title}`}</h2>
+
           <ComposedChart width={800} height={500} style={{maginTop: '100px'}} data={data} >
             <XAxis dataKey = {xAxis.dataIndex} >
               <Label value={xAxis.title} offset={0} position="insideBottom"/>
             </XAxis>
             <YAxis dataKey={Yvalue} domain = {[0, 'dataMax']}>
-              <Label  value={Ylabel} angle={-90} position="insideBottomLeft" />
+              <Label  value={Ylabel} angle={-90} position='insideLeft' />
             </YAxis>
-            {graphType === 'composite_graph' && <YAxis yAxisId="right" dataKey={Yvalue} orientation='right' tickLine={false} axisLine={false} domain={[0, 100]} >
-              <Label value={Ylabel} angle={-90} position="insideTopRight" />
+            {graphType === 'composite_graph' && 
+              <YAxis yAxisId="right" dataKey={Y2value} orientation='right' tickLine={false} axisLine={false} domain={[0, 100]} >
+                <Label value={Y2label} angle={-90} position='insideLeft' />
               </YAxis>
             }
             {((data && graphType === 'bar_graph') ||( data && graphType === 'composite_graph')) && 
               <Bar dataKey={graphType === 'composite_graph' ?  Y2value: Yvalue} barSize={60} fill="#1890ff" >
-                <LabelList dataKey={ Yvalue} position="insideTop"/>
+                <LabelList dataKey={ Yvalue} position='centerBottom' />
               </Bar>
             }
             {(graphType === 'line_graph' || graphType === 'composite_graph') && 
@@ -141,7 +148,7 @@ const [graphType, setGraphType] = useState('line_graph')
           </ComposedChart>            
         </div>
 
-        <Select defaultValue="Graph type" style={{width: 120}} onChange={handleGraphTypeChange}>
+        <Select defaultValue="Line Graph" style={{width: 120}} onChange={handleGraphTypeChange}>
           <Option value="composite_graph">Composite Graph</Option>
           <Option value="bar_graph">Bar Graph</Option>
           <Option value="line_graph">Line Graph</Option>
